a00b85cce4a5354f8a7cdf6ff7985cdab1078bc3
Hudcan <hudcan@sina.com>
2021-11-01 16:34:44 +0800

    修改pom支持中央仓库发布

--- src/main/java/io/github/toohandsome/classassist/core/IClassPatch.java
+++ src/main/java/io/github/toohandsome/classassist/core/IClassPatch.java
@@ -11,25 +11,23 @@ public interface IClassPatch {
     /**
      * 返回 需要修改的方法
      *
-     * @return
      */
     ArrayList<MethodMeta> getEditMethodList();
 
     /**
      * 返回 需要新增的方法
-     * @return
+     *
      */
     ArrayList<MethodMeta> getAddMethodList();
 
     /**
      * 返回需要新增的字段
-     * @return
+     *
      */
     List<String> getAddFieldList();
 
     /**
      * 返回需要导入的包
-     * @return
      */
     List<String> getImprotPackages();
 }
